0c45a1eddbe0bffbe4ac0d1945a4da130d31e8ea
iamalviaringo <iamalvinaringo@gmail.com>
2016-12-16 09:11:41 +0300

    Final Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#PASSIONATE EXPRESSION 3
+
+Passionate Expression is an event that offers a platform for Individuals to showcase their talent. This is the first draft of the website. It is a one page website that includes the following sections:
+
+1. Registration forms
+2. Gallery
+3. Blog
+4. Countdown Timer
+5. Background Video
+
+In the making of the site, we applied the knowledge learnt in the 5 weeks ie HTML, CSS and JS. WE also explored further and got deeper understating on the concepts.
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+
+
+$(document).ready(function() {
+    function countdown() {
+        var now = new Date();
+        var eventDate = new Date(2016, 11, 25);
+        var currentTime = now.getTime();
+        var eventTime = eventDate.getTime();
+        var remTime = eventTime - currentTime;
+        var s = Math.floor(remTime / 1000);
+        var m = Math.floor(s / 60);
+        var h = Math.floor(m / 60);
+        var d = Math.floor(h / 24);
+        h %= 24;
+        m %= 60;
+        s %= 60;
+
+        h = (h < 10) ? "0" + h : h;
+        m = (m < 10) ? "0" + m : m;
+        s = (s < 10) ? "0" + s : s;
+
+
+        document.getElementById("days").textContent = d;
+        document.getElementById("days").InnerText = d;
+
+        document.getElementById("hours").textContent = h;
+        document.getElementById("minutes").textContent = m;
+        document.getElementById("seconds").textContent = s;
+
+        setTimeout(countdown, 1000);
+    };
+
+    countdown();
+
+    $("form#subscribe").submit(function  () {
+      event.preventDefault();
+
+      var input=$("#email").val();
+
+      $(".unsubscribed").hide();
+      $(".subscribed").append(input + " has been successfully subscribed to our newsletter. Thank you.")
+      $(".subscribed").show();
+    });
+
+
+});
